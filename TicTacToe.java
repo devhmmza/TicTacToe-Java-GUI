c2b533dd2b62649d9197fda66dea99bd0a3f06ce
Hamza <hmmza.py@gmail.com>
2025-06-12 21:22:15 +0800

    TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TicTacToe extends JFrame implements ActionListener {
+    JButton[][] buttons = new JButton[3][3];
+    boolean xTurn = true;
+
+    public TicTacToe() {
+        setTitle("Tic Tac Toe");
+        setSize(400, 400);
+        setLayout(new GridLayout(3, 3));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setFont(new Font("Arial", Font.BOLD, 60));
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JButton b = (JButton) e.getSource();
+        if (!b.getText().equals("")) return;
+        b.setText(xTurn ? "X" : "O");
+        if (checkWin()) {
+            JOptionPane.showMessageDialog(this, (xTurn ? "X" : "O") + " wins!");
+            resetBoard();
+        } else if (isDraw()) {
+            JOptionPane.showMessageDialog(this, "It's a draw!");
+            resetBoard();
+        } else {
+            xTurn = !xTurn;
+        }
+    }
+
+    boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().equals("") &&
+                buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                buttons[i][1].getText().equals(buttons[i][2].getText())) return true;
+            if (!buttons[0][i].getText().equals("") &&
+                buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                buttons[1][i].getText().equals(buttons[2][i].getText())) return true;
+        }
+        if (!buttons[0][0].getText().equals("") &&
+            buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+            buttons[1][1].getText().equals(buttons[2][2].getText())) return true;
+        if (!buttons[0][2].getText().equals("") &&
+            buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+            buttons[1][1].getText().equals(buttons[2][0].getText())) return true;
+        return false;
+    }
+
+    boolean isDraw() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (buttons[i][j].getText().equals("")) return false;
+        return true;
+    }
+
+    void resetBoard() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                buttons[i][j].setText("");
+        xTurn = true;
+    }
+
+    public static void main(String[] args) {
+        new TicTacToe();
+    }
+}
